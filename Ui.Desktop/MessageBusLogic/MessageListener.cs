81170dae6113b2d6a1a025571f06fa23b1cc78a6
Nils-Broder Thomsen <nith4688@stud.hs-flensburg.de>
2024-11-18 15:33:45 +0100

    alles

---
+++ Ui.Desktop/MessageBusLogic/MessageListener.cs
@@ -0,0 +1,30 @@
+﻿using De.HsFlensburg.ClientApp112.Services.MessageBus;
+using De.HsFlensburg.ClientApp112.Logic.Ui.MessageBusMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp112.Ui.Desktop.MessageBusLogic
+{
+    class MessageListener
+    {
+        public bool BindableProperty => true;
+        public MessageListener()
+        {
+            InitMessenger();
+        }
+        private void InitMessenger()
+        {
+            ServiceBus.Instance.Register<OpenNewClientWindowMessage>
+                (this, OpenNewClientWindow
+                );
+        }
+        private void OpenNewClientWindow()
+        {
+            NewClientWindow myWindow = new NewClientWindow();
+            myWindow.ShowDialog();
+        }
+    }
+}
